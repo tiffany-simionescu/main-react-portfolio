96fcbc626d5b3cba18dce8eca91b6cd97db1f8df
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-05-17 21:03:10 -0400

    Initial Commit - Basic Navbar setup.

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import { 
+  Nav, 
+  NavbarContainer, 
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Portfolio</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="portfolio">Portfolio</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="resume">Resume</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar;
