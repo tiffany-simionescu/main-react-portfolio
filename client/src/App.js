a26d22568651009294154ef0ea64e51f972d82ff
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-05-17 22:42:26 -0400

    Added sidebar styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
       <Navbar />
     </div>
   );
---
+++ client/src/components/Sidebar/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SidebarRoute,
+  SideBtnWrap,
+} from './SidebarElements';
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">
+            About
+          </SidebarLink>
+          <SidebarLink to="portfolio">
+            Portfolio
+          </SidebarLink>
+          <SidebarLink to="resume">
+            Resume
+          </SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar
